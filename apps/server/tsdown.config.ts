a11d39be6783099c20e205084458861f6bacb8fe
Pandit Pawar <iampandit.in@gmail.com>
2026-04-20 21:20:49 +0530

    delete the turborepo

--- apps/server/tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsdown";
-
-export default defineConfig({
-  entry: "./src/index.ts",
-  format: "esm",
-  outDir: "./api",
-  clean: true,
-  noExternal: [/@rooms\/.*/],
-});
